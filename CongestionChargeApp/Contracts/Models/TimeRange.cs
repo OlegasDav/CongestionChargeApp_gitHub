cda909a689942864e1efae18effe292ae8552cab
Oleg <oldav0722@gmail.com>
2021-11-06 22:36:43 +0200

    Updated TimeRange class

--- CongestionChargeApp/Contracts/Models/TimeRange.cs
+++ CongestionChargeApp/Contracts/Models/TimeRange.cs
@@ -4,10 +4,10 @@ namespace Contracts.Models
 {
     public class TimeRange
     {
-        public static TimeSpan point7Am { get; set; } = TimeSpan.FromHours(7);
+        public static TimeSpan point7Am = TimeSpan.FromHours(7);
 
-        public static TimeSpan point12Pm { get; set; } = TimeSpan.FromHours(12);
+        public static TimeSpan point12Pm = TimeSpan.FromHours(12);
 
-        public static TimeSpan point7Pm { get; set; } = TimeSpan.FromHours(19);
+        public static TimeSpan point7Pm = TimeSpan.FromHours(19);
     }
 }
