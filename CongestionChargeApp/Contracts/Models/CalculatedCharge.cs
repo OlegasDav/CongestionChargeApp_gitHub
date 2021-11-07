92477a72145329e5ae9801085e0bc9e2ac8888ff
Oleg <oldav0722@gmail.com>
2021-11-07 13:22:01 +0200

    Separated Print and Calculated methods

---
+++ CongestionChargeApp/Contracts/Models/CalculatedCharge.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Contracts.Models
+{
+    public class CalculatedCharge
+    {
+        public decimal ChargeForAm { get; set; }
+
+        public decimal ChargeForPm { get; set; }
+
+        public decimal TotalCharge { get; set; }
+
+        public TimeSpan AmHours { get; set; }
+
+        public TimeSpan PmHours { get; set; }
+    }
+}
